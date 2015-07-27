3a70fa933050eb5ebfd53d408470fbc6058d933c
ray.feng <figo10203@163.com>
2015-07-27 11:44:45 +0800

    release the project
    release the project

--- sdk/src/main/java/cn/beecloud/BeeCloud.java
+++ sdk/src/main/java/cn/beecloud/BeeCloud.java
@@ -4,8 +4,8 @@ public class BeeCloud {
     /**
      * BeeCloud SDK release version.
      */
-    public static final String kBeeCloudVersionString = "1.0.2";
-    public static final double kBeeCloudVersionNumber = 1.02;
+    public static final String kBeeCloudVersionString = "2.0.0";
+    public static final double kBeeCloudVersionNumber = 2.00;
 
     /**
      * 设置appID和appSecret，在程序开始时运行一遍即可
