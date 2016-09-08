dcb495f0c963ac10119b89446763ea4769ea902c
ray_feng <figo10203@163.com>
2016-09-08 15:52:04 +0800

    update doc

--- sdk/src/main/java/cn/beecloud/BeeCloud.java
+++ sdk/src/main/java/cn/beecloud/BeeCloud.java
@@ -11,8 +11,8 @@ public class BeeCloud {
     /**
      * BeeCloud JAVA SDK release version.
      */
-    public static final String kBeeCloudVersionString = "3.4.6";
-    public static final double kBeeCloudVersionNumber = 3.46;
+    public static final String kBeeCloudVersionString = "3.4.7";
+    public static final double kBeeCloudVersionNumber = 3.47;
 
     /**
      * 设置appID、appSecret、masterSecret，在程序开始时运行一遍即可
